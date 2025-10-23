91fc2e476d9fe515493ea7ca56daea59ce9038d0
surp1us <2985226239@qq.com>
2025-10-23 15:25:52 +0800

    update: test middleware

--- tests/example/app.py
+++ tests/example/app.py
@@ -1,7 +1,8 @@
+from typing import Callable
 import tests.example_grpc_proto.example_pb2_grpc as example_pb2_grpc
 import tests.example_grpc_proto.example_pb2 as example_pb2
 from dataclasses import dataclass
-from src.grpc_framework import GRPCFramework, GRPCFrameworkConfig
+from src.grpc_framework import GRPCFramework, GRPCFrameworkConfig, BaseMiddleware, Request
 from tests.example.user_service import UserService
 from tests.example.profile_service import service as profile_service
 
@@ -14,9 +15,18 @@ class SimpleService(example_pb2_grpc.SimpleServiceServicer):
         yield example_pb2.SimpleResponse(result='success', success=True, error_message='')
 
 
+class LoggerMiddleware(BaseMiddleware):
+
+    async def dispatch(self, request: Request, call_next: Callable):
+        print('received the request ->', request)
+        return await call_next(request)
+
+
 config = GRPCFrameworkConfig.from_module('tests.example.config')
 
 app = GRPCFramework(config=config)
+
+app.add_middleware(LoggerMiddleware)
 
 app.add_service(UserService)
 app.add_service(profile_service)
