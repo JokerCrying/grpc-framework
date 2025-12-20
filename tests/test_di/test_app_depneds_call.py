451787c4391d531df9c59f5581c2c90f44652e0b
surp1us <2985226239@qq.com>
2025-12-20 23:35:55 +0800

    test(di): Add RootService and UserService related interface call tests
    - Added tests for health_check, complex_op, and redis interface calls to RootService
    - Added redis_test2 and get_user interface call tests for UserService
    - Expanded stream_users streaming response testing for UserService
    - Existing UserService redis_test test cases are retained and invoked
    - Output the response results of all interface calls to verify the test effect

--- tests/test_di/test_app_depneds_call.py
+++ tests/test_di/test_app_depneds_call.py
@@ -15,7 +15,44 @@ client = GRPCClient(
 
 
 resp = client.unary_unary(
+    '/test.RootService/health_check',
+    request_data={}
+)
+print(resp)
+
+resp = client.unary_unary(
+    '/test.RootService/complex_op',
+    request_data={}
+)
+print(resp)
+
+resp = client.unary_unary(
+    '/test.RootService/redis',
+    request_data={}
+)
+print(resp)
+
+resp = client.unary_unary(
     '/test.UserService/redis_test',
     request_data={}
 )
 print(resp)
+
+resp = client.unary_unary(
+    '/test.UserService/redis_test2',
+    request_data={}
+)
+print(resp)
+
+resp = client.unary_unary(
+    '/test.UserService/get_user',
+    request_data={}
+)
+print(resp)
+
+
+for resp in client.unary_stream(
+    '/test.UserService/stream_users',
+    request_data={}
+):
+    print(resp)
